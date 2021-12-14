c45c6a01d2823341280eba1776644e0272b1f4c5
Vikash UI_GEMS <cvvkshcv@gmail.com>
2021-12-15 00:56:28 +0530

    added react-webcam recorder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import styled from 'styled-components';
+import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+
+const Button = styled.button`
+  background: unset;
+  border: unset;
+  cursor: pointer;
+  background: ${props => props.primary ? 'palevioletred' : '#eee'};
+  color: ${props => props.primary ? '#fff' : '#333'};
+  padding: .3rem 1rem;
+  box-sizing: border-box;
+`;
+
+const ButtonRed = styled(Button)`
+  border: 1px solid red;
+  color: red;
+`;
+
+const Container = styled.div`
+  width: ${props => props.width};
+  margin: 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>React app</h1>
+        <Container width="50%">
+          <Button primary as={Link} to="/">Hello</Button>
+          <Button>Hello</Button>
+          <ButtonRed as={Link} to="/home">Hello red</ButtonRed>
+        </Container>
+      </div>
+      <Routes>
+        <Route path="home" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import WebcamRecorder from "./WebcamRecorder";
+function Home() {
+  return (
+    <div>
+      <WebcamRecorder />
+    </div>
+  )
+}
+
+export default Home
